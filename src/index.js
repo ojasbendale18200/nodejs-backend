2e9d9d7e9187bb4e4937a8797d33ed87a6057e48
shivam.rai@perpetualny.com <shivam.rai@perpetualny.com>
2025-01-09 18:18:49 +0530

    established mongodb connection

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import { app } from "./app.js";
+import connectDB from "./db/db.js";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: ".env",
+});
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`⚙️   Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+  });
