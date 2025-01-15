1e9bf9859584aed3ec099e6ab893a5cf4cdb327f
shivam.rai@perpetualny.com <shivam.rai@perpetualny.com>
2025-01-15 17:51:14 +0530

    added Utils folder

--- src/app.js
+++ src/app.js
@@ -1,4 +1,17 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
 const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+  })
+);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 export { app };
